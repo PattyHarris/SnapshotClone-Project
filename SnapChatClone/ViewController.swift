8e654375772c9c5f9ad6027504ed162bf144fc3a
Patty Harris <patriciaharris@hotmail.com>
2017-08-24 13:06:47 -0700

    Initial Commit

---
+++ SnapChatClone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnapChatClone
+//
+//  Created by Patty Harris on 8/24/17.
+//  Copyright © 2017 Patty Harris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
