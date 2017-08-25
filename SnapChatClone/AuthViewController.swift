5184686fe14314504c8921c72b7cbb42181bab26
Unknown <patriciaharris@hotmail.com>
2017-08-25 14:30:42 -0700

    Committing changes before fixing project issues
    Getting some sore to AppDelegate runtime warning.
    Plus there's some XCode project issues with firebase.

--- SnapChatClone/AuthViewController.swift
+++ SnapChatClone/AuthViewController.swift
@@ -10,16 +10,59 @@ import UIKit
 
 class AuthViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    // Nick switches the labels on this - TBD...
+    @IBOutlet weak var topButton: UIButton!
+    @IBOutlet weak var bottomButton: UIButton!
+
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
+  
+    // Indicates whether we're logging in or signing up.
+    var loginMode : Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setLabelsAndText()
+    }
+    
+    func setLabelsAndText() {
+        if loginMode == true {
+            topLabel.text = Storyboard.loginLabel
+            topButton.titleLabel?.text = Storyboard.loginButton
+            
+            bottomLabel.text = Storyboard.signUpLabel
+            bottomButton.titleLabel?.text = Storyboard.signUpButton
+        }
+        else {
+            topLabel.text = Storyboard.signUpLabel
+            topButton.titleLabel?.text = Storyboard.signUpButton
+            
+            bottomLabel.text = Storyboard.loginLabel
+            bottomButton.titleLabel?.text = Storyboard.loginButton
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func topButtonDidTap(_ sender: Any) {
     }
-
+    
+    @IBAction func bottomButtonDidTap(_ sender: Any) {
+    }
+    
+    // Nested class Storyboard - used to set the text of the buttons and
+    // labels.
+    struct Storyboard {
+        
+        // Login label and button
+        static let loginLabel = "Login to start using SnapChatClone!"
+        static let loginButton = "Login"
+        
+        static let signUpLabel = "No account?  Sign up for free!"
+        static let signUpButton = "Sign Up"
+    }
 
 }
 
